dd5d7fb8a215ba84265fe047c8c3eb342fc1117f
jiawei-hong <zxc37984@gmail.com>
2022-02-07 19:53:31 +0800

    fix:introduce the wrong package

--- src/pages/Repository.js
+++ src/pages/Repository.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react/cjs/react.development";
 import { getRepository } from "../api";
 import Alert from '../components/Alert';
 import Repository from "../components/Repository";
