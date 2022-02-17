73ac5b2ea795344518f5cde25a371e44078dea8b
jiawei-hong <zxc37984@gmail.com>
2022-02-17 11:46:16 +0800

    fix:use navigate problem

--- src/pages/RepositoryList.js
+++ src/pages/RepositoryList.js
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUserProfile, getReposWithUsernameAndPage } from "../api";
 import Alert from "../components/Alert";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import RepositorySidebar from "../components/RepositoryList/Sidebar";
-import Repository from "../components/Repository";
+import RepositoryList from "../components/RepositoryList";
 
 function RepositoryListPage() {
   const params = useParams();
@@ -12,50 +12,58 @@ function RepositoryListPage() {
   const [maxRepositoryCount, setMaxRepositoryCount] = useState(0);
   const [page, setPage] = useState(1);
   const [repository, setRepository] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-  const getTenRepository = useCallback(async (username, page) => {
-    const getRepositoryWithPage = await getReposWithUsernameAndPage(username, page);
+  async function getRepository(page = 1) {
+    const getRepositoryWithPage = await getReposWithUsernameAndPage(
+      params.username,
+      page
+    );
 
     if (!getRepositoryWithPage.status) {
-      setRepository(repos => [
-        ...repos,
-        ...getRepositoryWithPage
-      ])
+      setRepository((repos) => [...repos, ...getRepositoryWithPage]);
     } else {
       setError(getRepositoryWithPage.data.message);
     }
-  }, []);
-
-  const getProfile = useCallback(async username => {
-    const profile = await getUserProfile(username);
-
-    if (!profile.status) {
-      setMaxRepositoryCount(profile.public_repos);
-      setProfile(profile);
-      setRepository([])
-      setPage(1)
-    } else {
-      setError(`This user was ${profile.data.message}`);
-    }
-  }, [])
+  }
 
   useEffect(() => {
-    getProfile(params.username)
-  }, [params.username, getProfile]);
+    async function getProfile() {
+      const profile = await getUserProfile(params.username);
 
-  useEffect(() => {
-    if (Object.keys(profile).length > 0) {
-      getTenRepository(params.username, page);
+      if (!profile.status) {
+        setMaxRepositoryCount(profile.public_repos);
+        setProfile(profile);
+      } else {
+        setError(`This user was ${profile.data.message}`);
+      }
     }
-  }, [params.username, page, getTenRepository, profile]);
+
+    getProfile();
+
+    const storageName = localStorage.getItem("name");
+
+    if (storageName && storageName !== params.username) {
+      setRepository([]);
+      setError("");
+      setPage(1);
+    }
+
+    getRepository();
+
+    localStorage.setItem("name", params.username);
+  }, [params.username]);
 
   async function scroll(e) {
     const { scrollHeight, clientHeight, scrollTop } = e.target;
 
-    if (scrollHeight === clientHeight + scrollTop && repository.length < maxRepositoryCount) {
+    if (
+      scrollHeight !== clientHeight &&
+      scrollHeight === clientHeight + scrollTop &&
+      repository.length < maxRepositoryCount
+    ) {
       setPage(page + 1);
-      getTenRepository();
+      getRepository(page + 1);
     }
   }
 
@@ -63,39 +71,31 @@ function RepositoryListPage() {
     <React.Fragment>
       <Navbar />
 
-      {
-        error ? (
-          <div className="container mx-auto mt-3">
-            <Alert variant="error" text={error} />
-          </div>
-        )
-          : (
-            <div
-              className="container grid grid-cols-[.25fr_.75fr] mx-auto pt-5 repository-list-cotainer h-screen overflow-y-scroll px-5"
-              onScroll={e => scroll(e)}
-            >
-              <RepositorySidebar profile={profile} />
+      {error ? (
+        <div className="container mx-auto mt-3">
+          <Alert variant="error" text={error} />
+        </div>
+      ) : (
+        <div
+          className="container grid grid-cols-[.25fr_.75fr] mx-auto pt-5 repository-list-cotainer h-screen overflow-y-scroll px-5"
+          onScroll={(e) => scroll(e)}
+        >
+          <RepositorySidebar profile={profile} />
 
-              <div>
-                {
-                  repository.length > 0 ? (
-                    <React.Fragment>
-                      {
-                        repository.map((repo,i) => (
-                          <Repository repo={repo} settings={{ inList: true }} key={i} />
-                        ))
-                      }
-                    </React.Fragment>
-                  ) : (
-                    <Alert variant="info" text={`${profile.login} doesn’t have any repositories.`} />
-                  )
-                }
-              </div>
-            </div>
-          )
-      }
+          <div>
+            {repository.length > 0 ? (
+              <RepositoryList repository={repository} />
+            ) : (
+              <Alert
+                variant="info"
+                text={`${profile.login} doesn’t have any repositories.`}
+              />
+            )}
+          </div>
+        </div>
+      )}
     </React.Fragment>
-  )
+  );
 }
 
 export default RepositoryListPage;
